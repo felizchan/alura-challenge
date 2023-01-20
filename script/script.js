162d0fde65e0262820cd5dd541d4a36539d907a6
felizchan <kakarotokz.v3@hotmail.com>
2023-01-20 19:45:13 -0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,28 @@
+var textInput = document.querySelector("#input");
+
+var outInput = document.querySelector("#output");
+
+function criptografar(){
+
+  var texto = textInput.value;
+
+  var resultCripto = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g,"ai").replace(/o/g, "ober").replace(/u/g, "ufat")
+
+  document.getElementById('output').innerHTML = resultCripto;
+}
+
+function descriptografar(){
+  var texto = textInput.value;
+
+  var resultDescripto = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+  
+
+  document.getElementById('output').innerHTML =  resultDescripto;
+}
+
+  function copiar() {
+    var textoCop = document.getElementById('output');
+    textoCop.select();
+    document.execCommand('copy');
+    alert("Texto copiado.");
+}
