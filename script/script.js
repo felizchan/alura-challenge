e1939732da83ac02e3f786e1cc2583e105530228
felizchan <kakarotokz.v3@hotmail.com>
2023-01-25 17:55:19 -0300

    alterações na função copiar2

--- script/script.js
+++ script/script.js
@@ -1,27 +1,35 @@
-var textInput = document.querySelector("#input");
+var textInput = document.querySelector('#input');
 
-var outInput = document.querySelector("#output");
+var outInput = document.querySelector('#output');
 
-function criptografar(){
-
+function criptografar() {
   var texto = textInput.value;
 
-  var resultCripto = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g,"ai").replace(/o/g, "ober").replace(/u/g, "ufat")
+  var resultCripto = texto
+    .replace(/e/g, 'enter')
+    .replace(/i/g, 'imes')
+    .replace(/a/g, 'ai')
+    .replace(/o/g, 'ober')
+    .replace(/u/g, 'ufat');
 
   document.getElementById('output').innerHTML = resultCripto;
 }
 
-function descriptografar(){
+function descriptografar() {
   var texto = textInput.value;
 
-  var resultDescripto = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
-  
+  var resultDescripto = texto
+    .replace(/enter/g, 'e')
+    .replace(/imes/g, 'i')
+    .replace(/ai/g, 'a')
+    .replace(/ober/g, 'o')
+    .replace(/ufat/g, 'u');
 
-  document.getElementById('output').innerHTML =  resultDescripto;
+  document.getElementById('output').innerHTML = resultDescripto;
 }
 
-  function copiar() {
-    var textoCop = document.getElementById('output');
-    navigator.clipboard.writeText(textoCop.value);
-    alert("Texto copiado.");
+function copiar() {
+  var textoCop = document.getElementById('output').value;
+  navigator.clipboard.writeText(textoCop);
+  alert('Texto copiado.');
 }
